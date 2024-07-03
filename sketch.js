0460b8228706020446e1e030f874c027abea1208
M1notaure <aubut.arnaud@gmail.com>
2024-07-03 15:59:03 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,6 +1,7 @@
 
 function preload() {
-  spriteSheet = loadImage('Assets/Soldier/Soldier/Soldier.png');
+  enemySheet = loadImage('Assets/Orc/Orc/Orc.png')
+  characterSheet = loadImage('Assets/Soldier/Soldier/Soldier.png');
   tilesSheet = loadImage('Assets/sprites/world_tileset.png')
   fontText = loadFont('Assets/fonts/PixelOperator8.ttf')
 }
@@ -68,11 +69,34 @@ function setup() {
   world.gravity.y = 9.81;
   allSprites.pixelPerfect = true;
 
+  goblin = new Sprite(100, 113)
+  goblin.width = 10
+  goblin.height = 12
+  goblin.collider = 'none'
+  goblin.tile = 'e'
+  goblin.spriteSheet = enemySheet
+  goblin.rotationLock = true
+  goblin.addAnis({
+    run: {w: 100, h:100, row:1, frames: 8, frameDelay: 40},
+    idle: {w: 100, h: 100, row:0, frames:6, frameDelay: 25},
+    attack01: { w: 100, h: 100, row: 2, frames: 6, frameDelay: 5 },
+    attack02: { w: 100, h: 100, row: 3, frames: 6, frameDelay: 10 },
+    hurt: { w: 100, h: 100, row: 4, frames: 4},
+    death: { w: 100, h: 100, row: 5, frames: 4, frameDelay: 25}
+  })
+
+  goblin.changeAni('run')
+
+  goblin.velocity.x = 1
+  if(goblin.collides('dirt'))
+
+
+
   player = new Sprite(3, 96);
   player.width = 10;
   player.height = 12;
   player.bounciness = 0
-  player.spriteSheet = spriteSheet;
+  player.spriteSheet = characterSheet;
   player.rotationLock = true;
   player.addAnis({
     run: { w: 100, h: 100, row: 1, frames: 8 },
@@ -84,7 +108,7 @@ function setup() {
     death: { w: 100, h: 100, row: 6, frames: 4, frameDelay: 30 }
   })
   player.anis.offset.y = 0.5
-  // allSprites.debug = true
+
 
   player.changeAni('idle')
 
@@ -99,13 +123,12 @@ function setup() {
   )
 
 
-   // g = grass
+  // g = grass
   // d = dirt
   // r = rock
   // c = underground_rock
   // s = sign
   // b = bush
-  // f = flowered_bush 
   new Tiles(
     [
       '                               s ',
@@ -114,15 +137,17 @@ function setup() {
       '    ggg                          ',
       '               g        ggg   ggg',
       'gg        b   gggg  gg           ',
-      '        gggg   ddd       s    ggg',
-      '   ggg  b               gg       ',
-      'gggdddggggggggg   ggggggggg  gggg',
+      '        gggg     d       s    ggg',
+      '   ggd  b     d         gg       ',
+      'gggdddggggggggd   ggggggggg  gggg',
       'dddddddddddddddgggddddddddd  dddd',
       'ddddddddddddddddddddddddddd  dddd',
     ],
     0, 0,
     16, 16
   )
+
+  // allSprites.debug = true
 }
 
 let canJump = true;
@@ -181,10 +206,10 @@ async function draw() {
     attack03InProgress = false
   }
 
-  // textFont(fontText)
-  // textSize(9)
-  // text('X:' + player.x, 10, 20)
-  // text('Y:' + player.y, 10, 40)
+  textFont(fontText)
+  textSize(9)
+  text('X:' + player.x, 10, 20)
+  text('Y:' + player.y, 10, 40)
 
   camera.x = player.x
   camera.y = player.y
